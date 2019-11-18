b145bcc8668d13d1d29d021df7b287d0055c4522
Garrett Trott <garretttrott@gmail.com>
2019-11-18 01:29:06 -0800

    fix event listener bug

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Function to find the list item indexes of a given page then hides other indexes
 
-  function showPage(list, pageNumber) {
+  const showPage = (list, pageNumber) => {
     const startIndex = pageNumber * ItemsPerPage - ItemsPerPage;
     const endIndex = pageNumber * ItemsPerPage;
     for (let i = 0; i < list.length; i++) {
@@ -16,11 +16,11 @@ document.addEventListener("DOMContentLoaded", () => {
         list[i].style.display = "none";
       }
     }
-  }
+  };
 
   // Function to generate, append, and add functionality to the pagination buttons.
 
-  function appendPageLinks(list) {
+  const appendPageLinks = list => {
     const numberOfPages = list.length / ItemsPerPage;
     const pageDiv = document.querySelector(".page");
     const div = document.createElement("div");
@@ -35,22 +35,23 @@ document.addEventListener("DOMContentLoaded", () => {
       li.appendChild(a);
       a.href = "#";
       ul.appendChild(li);
-    }
-    ul.firstChild.firstChild.className = "active";
 
-    ul.addEventListener("click", e => {
-      const pageSelected = e.target.textContent;
-      const links = ul.querySelectorAll("a");
-      showPage(studentList, pageSelected);
-      e.target.className = "active";
-      for (let i = 0; i < links.length; i++) {
-        if (links[i] !== e.target) {
-          links[i].className = "";
+      a.addEventListener("click", e => {
+        const pageSelected = e.target.textContent;
+        const links = ul.querySelectorAll("a");
+        showPage(studentList, pageSelected);
+        e.target.className = "active";
+        for (let i = 0; i < links.length; i++) {
+          if (links[i] !== e.target) {
+            links[i].className = "";
+          }
         }
-      }
-    });
+      });
+    }
+
+    ul.firstChild.firstChild.className = "active";
     pageDiv.appendChild(div);
-  }
+  };
 
   appendPageLinks(studentList);
   showPage(studentList, 1);
