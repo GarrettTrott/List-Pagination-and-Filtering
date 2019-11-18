5a17b8a5056dc6c0f2dc2b8247fb2b46e9759d4f
Garrett Trott <garretttrott@gmail.com>
2019-11-17 22:44:19 -0800

    add event listener for page buttons

--- js/script.js
+++ js/script.js
@@ -1,42 +1,50 @@
-// Global variables for list elements and items to be displayed per page
+document.addEventListener("DOMContentLoaded", () => {
+  // Global variables for list elements and items to be displayed per page
 
-const studentList = document.querySelectorAll(".student-item");
-const ItemsPerPage = 10;
+  const studentList = document.querySelectorAll(".student-item");
+  const ItemsPerPage = 10;
 
-// Function to find the list item indexes of a given page then hides other indexes
+  // Function to find the list item indexes of a given page then hides other indexes
 
-function showPage(list, pageNumber) {
-  const startIndex = pageNumber * ItemsPerPage - ItemsPerPage;
-  const endIndex = pageNumber * ItemsPerPage;
-  for (let i = 0; i < list.length; i++) {
-    if (i >= startIndex && i < endIndex) {
-      list[i].style.display = "";
-    } else {
-      list[i].style.display = "none";
+  function showPage(list, pageNumber) {
+    const startIndex = pageNumber * ItemsPerPage - ItemsPerPage;
+    const endIndex = pageNumber * ItemsPerPage;
+    for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+        list[i].style.display = "";
+      } else {
+        list[i].style.display = "none";
+      }
     }
   }
-}
 
-// Function to generate, append, and add functionality to the pagination buttons.
+  // Function to generate, append, and add functionality to the pagination buttons.
 
-function appendPageLinks(list) {
-  const numberOfPages = list.length / ItemsPerPage;
-  const pageDiv = document.querySelector(".page");
-  const div = document.createElement("div");
-  const ul = document.createElement("ul");
+  function appendPageLinks(list) {
+    const numberOfPages = list.length / ItemsPerPage;
+    const pageDiv = document.querySelector(".page");
+    const div = document.createElement("div");
+    const ul = document.createElement("ul");
+    div.className = "pagination";
+    div.appendChild(ul);
 
-  div.className = "pagination";
-  div.appendChild(ul);
+    for (let i = 0; i < numberOfPages; i++) {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.textContent = i + 1;
+      li.appendChild(a);
+      a.href = "#";
+      ul.appendChild(li);
+    }
+    ul.firstChild.firstChild.className = "active";
 
-  for (i = 0; i < numberOfPages; i++) {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    a.textContent = i + 1;
-    li.appendChild(a);
-    ul.appendChild(li);
+    ul.addEventListener("click", e => {
+      const pageSelected = e.target.textContent;
+      showPage(studentList, pageSelected);
+    });
+    pageDiv.appendChild(div);
   }
 
-  pageDiv.appendChild(div);
-}
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+  appendPageLinks(studentList);
+  showPage(studentList, 1);
+});
